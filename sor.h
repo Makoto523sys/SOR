540319287041711f94ce62c5b0de54e10849afed
Makoto <ookunimasaharu@gmail.com>
2020-08-22 17:48:22 +0900

    Add pivot function

--- sor.h
+++ sor.h
@@ -7,35 +7,35 @@
 #include <algorithm>
 #include <vector>
 
-void sor(std::vector<std::vector<double>>&, std::vector<double>&, std::vector<double>&);
+void sor(std::vector<std::vector<double>>&, std::vector<double>&, std::vector<double>&, const double);
 bool is_convergence(const std::vector<double>&, const std::vector<double>&);
 void show_problem(const std::vector<std::vector<double>>&, std::vector<double>&);
+void pivot(const int row, std::vector<std::vector<double>>& A, std::vector<double>& b);
 
 /******************************************
  * sor method for linear problem
  * when you use this method,
  * osr(std::vector<std::vector<double>>& coefficient_matrix, std::vector<double>& X, std::vector<double>& b)
  ******************************************/
-inline void sor(std::vector<std::vector<double>>& A, std::vector<double>& x, std::vector<double>&  b){
-	const double omega = 1.1;
+inline void sor(std::vector<std::vector<double>>& A, std::vector<double>& x, std::vector<double>&  b, const double omega){
 	unsigned int iter = 0;
 	int rows = A.size();//行
 	int cols = A[0].size();//列
 	std::fill(x.begin(), x.end(), 0);
 	std::vector<double> prex(x.size());
 	//prex.resize(rows);
-	double tmp = 0.0f;
 	//std::copy(x.begin(), x.end(), prex.begin());
 	bool did_finish = false;
 
 	while(!did_finish){
 		for(int i = 0; i < rows; i++){
+			pivot(i, A, b);
 			double tmp = 0;
 			for(int j = 0; j < cols; j++)
 			       if(j != i) tmp += A[i][j] * x[j];
 			tmp = (b[i] - tmp) / A[i][i];
 			std::copy(x.begin(), x.end(), prex.begin());
-			x[i] = x[i] + omega * (tmp - x[i]);
+			x[i] = omega * tmp + (1.0 - omega) * x[i];
 		}
 		did_finish = is_convergence(x, prex);
 		iter++;
@@ -59,9 +59,28 @@ inline void show_problem(const std::vector<std::vector<double>>& A, std::vector<
 	int cols = A[0].size();
 	for(int i = 0; i < rows; i++){
 		for(int j = 0; j < cols - 1; j++){
-			std::cout << A[i][j] << "x_" << j+1 << " + ";
+			std::cout << A[i][j] << " x_" << j+1 << " + ";
 		}
 		std::cout << A[i][A[i].size() - 1] << "x_" << cols << " = " << b[i] << std::endl;
+	}
+}
+
+void pivot(const int row, std::vector<std::vector<double>>& A, std::vector<double>& b){
+	int n = b.size();
+	double max_diagonal = std::abs(A[row][row]);
+	double max_row = row;
+	for(int i = row; i < n; i++){
+		if(max_diagonal < std::abs(A[i][row])){
+			max_row = i;
+			max_diagonal = std::abs(A[i][row]);
+		}
+	}
+	if(max_row == row) return;
+	else{
+		std::vector<double> tmp(n);
+		std::copy(A[row].begin(), A[row].end(), tmp.begin());
+		std::copy(A[max_row].begin(), A[max_row].end(), A[row].begin());
+		std::copy(tmp.begin(), tmp.end(), A[max_row].begin());
 	}
 }
 
